f1439a99805989896c1b7f34c3f803c618ef9ac2
Justin <a>
2022-09-08 09:24:10 +0700

    hh

--- app/src/main/java/com/example/ugdnyakawan/HomeActivity.kt
+++ app/src/main/java/com/example/ugdnyakawan/HomeActivity.kt
@@ -1,15 +1,9 @@
 package com.example.ugdnyakawan
 
-import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.widget.Button
-import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
 
